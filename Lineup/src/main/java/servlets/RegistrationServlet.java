038544cd8cafaab761e24886878688e4918737ed
C8linR <45794604+c8linr@users.noreply.github.com>
2021-06-25 13:33:21 -0400

    test

--- Lineup/src/main/java/servlets/RegistrationServlet.java
+++ Lineup/src/main/java/servlets/RegistrationServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/RegistrationServlet")
 public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
