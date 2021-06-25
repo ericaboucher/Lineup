0892d84c729ce220bb52e5e7a62ce8e7f644f1b0
ericaboucher <84082683+ericaboucher@users.noreply.github.com>
2021-06-25 14:27:31 -0400

    deleted duplications

--- Lineup/src/main/java/beans/Guardian.java
+++ Lineup/src/main/java/beans/Guardian.java
@@ -26,28 +26,5 @@ public class Guardian extends User {
         children.add(child);
         return true;
     }
-}
-    private ArrayList<Integer> children;
-    
-    // Constructors
-    public Guardian(String email, String pass) {
-        super(email, pass);
-        children = new ArrayList<Integer>();
-    }
-    public Guardian(String email, String pass, String fName, String lName){
-        super(email, pass, fName, lName);
-        children = new ArrayList<Integer>();
-    }
-    
-    // Modifiers
-    public boolean signChildIn(Integer child) {
-        return children.contains(child);
-    }
-    public boolean signChildOut(Integer child) {
-        return children.contains(child);
-    }
-    public boolean addChild(Integer child) {
-        children.add(child);
-        return true;
-    }
+
 }
