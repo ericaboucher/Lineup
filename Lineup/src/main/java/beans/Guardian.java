f61266d7ed82561e410f567ecd7cbf911ff6121a
C8linR <45794604+c8linr@users.noreply.github.com>
2021-06-25 14:04:02 -0400

    Implement Guardian class

--- Lineup/src/main/java/beans/Guardian.java
+++ Lineup/src/main/java/beans/Guardian.java
@@ -1,5 +1,29 @@
 package beans;
 
-public class Guardian extends User {
+import java.util.ArrayList;
 
+public class Guardian extends User {
+    private ArrayList<Integer> children;
+    
+    // Constructors
+    public Guardian(String email, String pass) {
+        super(email, pass);
+        children = new ArrayList<Integer>();
+    }
+    public Guardian(String email, String pass, String fName, String lName){
+        super(email, pass, fName, lName);
+        children = new ArrayList<Integer>();
+    }
+    
+    // Modifiers
+    public boolean signChildIn(Integer child) {
+        return false;
+    }
+    public boolean signChildOut(Integer child) {
+        return false;
+    }
+    public boolean addChild(Integer child) {
+        children.add(child);
+        return true;
+    }
 }
