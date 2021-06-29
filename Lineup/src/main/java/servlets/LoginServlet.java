b1e8cc710c33098a475ae47baabd56769acaddc2
C8linR <45794604+c8linr@users.noreply.github.com>
2021-06-29 10:35:19 -0400

    fix login servlet again

--- Lineup/src/main/java/servlets/LoginServlet.java
+++ Lineup/src/main/java/servlets/LoginServlet.java
@@ -1,3 +1,8 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -5,7 +10,6 @@ import javax.servlet.http.HttpSession;
 
 import dao.ApplicationDao;
 
-@WebServlet("/loginServlet")
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1787L;
@@ -13,10 +17,6 @@ public class LoginServlet extends HttpServlet {
     public LoginServlet() {
         super();
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//collect data from login form 
