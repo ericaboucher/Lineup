f613d394e8758fbcbba4bb85b7dc0e7d19f3370f
C8linR <45794604+c8linr@users.noreply.github.com>
2021-06-29 09:23:31 -0400

    Merge test

--- Lineup/src/main/java/beans/Guardian.java
+++ Lineup/src/main/java/beans/Guardian.java
@@ -10,6 +10,10 @@ public class Guardian extends User {
         super(email, pass);
         children = new ArrayList<Integer>();
     }
+    public Guardian(String email, String pass, String phoneNum) {
+        super(email, pass, phoneNum);
+        children = new ArrayList<Integer>();
+    }
     public Guardian(String email, String pass, String fName, String lName){
         super(email, pass, fName, lName);
         children = new ArrayList<Integer>();
