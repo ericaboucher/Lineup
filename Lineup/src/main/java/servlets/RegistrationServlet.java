5a845752eac529704462ac6e5ba226b6c812b117
C8linR <45794604+c8linr@users.noreply.github.com>
2021-06-29 10:36:37 -0400

    fix registration servlet

--- Lineup/src/main/java/servlets/RegistrationServlet.java
+++ Lineup/src/main/java/servlets/RegistrationServlet.java
@@ -1,12 +1,20 @@
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.MessageFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Guardian;
+import beans.User;
+import dao.ApplicationDao;
+
+@WebServlet ("/registrationServlet")
 public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -25,11 +33,56 @@ public class RegistrationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//collect data from form
 		String userType = request.getParameter("usertype");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		String firstName = request.getParameter("firstname");
 		String lastName = request.getParameter("lastname");
 		String phoneNumber = request.getParameter("phone");
+
+		if (userType == "Guardian") {
+			User guardian = new Guardian(email, password, userType, firstName, lastName, phoneNumber);
+		}else {
+			//create Staff user
+			System.out.println("Error. No way to create staff.");
+		}
+
+		//save user to db
+		ApplicationDao dao = new ApplicationDao();
+		int rows = dao.;//need method to insert data into mysql
+
+		//alert user with message if their registration was completed
+		String infoMessage = null;
+		if(rows == 0) {
+			infoMessage = "Sorry, an error occurred.";
+		} else {
+			infoMessage = "User created successfully!";
+		}
+
+		//write the message back to user
+		String page = getHTMLString(request.getServletContext().getRealPath("/register.html"), infoMessage);
+		response.getWriter().write(page);
 	}
+
+	public String getHTMLString(String filePath, String message) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		String line="";
+		StringBuffer buffer = new StringBuffer();
+		while((line=reader.readLine())!=null){
+			buffer.append(line);
+		}
+
+		reader.close();
+		String page = buffer.toString();
+
+		page = MessageFormat.format(page, message);
+
+		return page;		
+	}
+
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	// TODO Auto-generated method stub
+//}
+
 }
