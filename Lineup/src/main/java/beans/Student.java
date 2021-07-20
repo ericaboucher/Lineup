65f4863e2fdf7964b629e4b4bc92fe1fcbb5efaf
C8linR <45794604+c8linr@users.noreply.github.com>
2021-07-20 08:56:06 -0400

    Add constructor, signIn, signOut methods to Student class

--- Lineup/src/main/java/beans/Student.java
+++ Lineup/src/main/java/beans/Student.java
@@ -2,5 +2,32 @@ package beans;
 
 public class Student{
     private final UUID studentID;
+    private final String firstName;
+    private final String lastName;
+    private boolean signedIn;
     
+    public Student(String fName, String lName) {
+        studentId = new UUID();
+        firstName = fName;
+        lastName = lName;
+        signedIn = false;
+    }
+    
+    public boolean signIn() {
+        if(signedIn == true) {
+            return false;
+        } else {
+            signedIn = true;
+            return true;
+        }
+    }
+    
+    public boolean signOut() {
+        if(signedIn == false) {
+            return false;
+        } else {
+            signedIn = false;
+            return true;
+        }
+    }
 }
