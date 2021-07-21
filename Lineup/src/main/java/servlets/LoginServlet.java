d71292268825917cdcae427806f12c93c138d9fd
C8linR <45794604+c8linr@users.noreply.github.com>
2021-07-21 08:46:52 -0400

    Ensure no unintended changes

--- Lineup/src/main/java/servlets/LoginServlet.java
+++ Lineup/src/main/java/servlets/LoginServlet.java
@@ -1,9 +1,6 @@
 package servlets;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.MessageFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -12,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSession;
 
 import beans.User;
@@ -21,14 +17,15 @@ import dao.UserDao;
 @WebServlet(name = "loginServlet", urlPatterns = {"/loginServlet"})
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 17871L;
-    public String errorMessage = null;
 
     public LoginServlet() {
         super();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.html");
+        dispatcher.include(request, response);
+
         //collect data from login form 
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -50,30 +47,14 @@ public class LoginServlet extends HttpServlet {
             //context.setAttribute("user", currentUser);
             request.getRequestDispatcher("/home.html").forward(request, response);
         } else {
-            errorMessage = "Sorry, email or password is not valid. Please try again.";
-            System.out.println(errorMessage);
-            String page = getHTMLString(request.getServletContext().getRealPath("/index.html"), errorMessage);
-            response.getWriter().write(page);
+            String errorMessage = "Sorry, email or password is not valid. Please try again.";
+            request.setAttribute("error", errorMessage);
+            request.getRequestDispatcher("/index.html").forward(request, response);
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/index.html");
         dispatcher.include(request, response);
-    }
-    
-
-    public String getHTMLString(String filePath, String message) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String line="";
-        StringBuffer buffer = new StringBuffer();
-        while((line=reader.readLine())!=null){
-            buffer.append(line);
-        }
-        reader.close();
-        
-        String page = buffer.toString();
-        page = MessageFormat.format(page, message);
-        return page;        
     }
 }
